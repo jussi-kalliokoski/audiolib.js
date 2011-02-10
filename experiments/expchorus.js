ea4587eb06497cf4210065c19ef0e107a6d3a39d
jussi-kalliokoski <jussi.kalliokoski@gmail.com>
2011-02-10 09:21:15 +0200

    Added chorus and experiments

---
+++ experiments/expchorus.js
@@ -0,0 +1,44 @@
+// Depends on oscillator.
+
+function Chorus(sampleRate, delayTime, depth, freq){
+	var	self	= this,
+		buffer, bufferPos, sample;
+
+	self.delayTime	= delayTime || 30;
+	self.depth	= depth	|| 1;
+	self.freq	= freq || 3;
+
+	function calcCoeff(){
+		buffer = new Float32Array(self.sampleRate * 0.1);
+		bufferPos = 0;
+		var i, l = buffer.length;
+		for (i=0; i<l; i++){
+			buffer[i] = 0.0;
+		}
+	}
+
+	self.sampleRate = sampleRate;
+	self.osc = new Oscillator(sampleRate, freq);
+	self.calcCoeff = calcCoeff;
+	self.pushSample = function(s){
+		if (++bufferPos >= buffer.length){
+			bufferPos = 0;
+		}
+		buffer[bufferPos] = s;
+		self.osc.generate();
+
+		var delay = self.delayTime + self.osc.getMix() * self.depth;
+		delay *= self.sampleRate / 1000;
+		delay = bufferPos - Math.floor(delay);
+		while(delay < 0){
+			delay += buffer.length;
+		}
+
+		sample = buffer[delay];
+	};
+	self.getMix = function(){
+		return sample;
+	};
+
+	calcCoeff();
+}
