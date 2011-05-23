f619686a5ee635730d7b2184e53a44b4bdf7e81f
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-05-23 14:50:53 +0300

    V.0.4.1

--- js/wrapper-end.js
+++ js/wrapper-end.js
@@ -20,7 +20,7 @@ audioLib.LP12Filter	= LP12Filter;
 //Geneneration
 audioLib.Oscillator	= Oscillator;
 
-audioLib.version	= '0.4.0';
+audioLib.version	= '0.4.1';
 
 return audioLib;
 }).call(typeof exports === 'undefined' ? {} : this, this.window || global, Math);
