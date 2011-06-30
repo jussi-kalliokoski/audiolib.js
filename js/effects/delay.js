db5a77ac55936ececc7409c05453f7b93c187792
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-06-30 16:32:10 +0300

    Rewrite Delay, see issues #13, #9

--- js/effects/delay.js
+++ js/effects/delay.js
@@ -7,69 +7,98 @@
  * @param {number} time (Optional) Delay time (ms).
  * @param {number} feedback (Optional) Feedback (unsigned double).
 */
-function Delay(samplerate, time, feedback)
-{
-	var	self		= this,
-		bufferSize	= (!samplerate ? 44100 : samplerate) * 2,
-		buffer		= new Float32Array(bufferSize),
-		bufferPos	= 0,
-		speed		= 0,
-		prevTime	= 0,
-		sample		= 0.0,
-
-		floor		= Math.floor;
-
-	function calcCoeff(){
-		speed = bufferSize / self.samplerate / self.time * 1000;
-		prevTime = self.time;
-	}
-
-	function fillBuffer(sample, from, to){
-		var i = from;
-		while (i++ !== to){
-			if (i >= bufferSize){
-				i=0;
-			}
-			buffer[i] = sample;
-		}
-	}
-
-	function algo0(s){
-		buffer[bufferPos++] += s;
-		if (bufferPos > self.time / 1000 * self.samplerate){
-			bufferPos = 0;
-		}
-		buffer[bufferPos] = buffer[bufferPos] * self.feedback;
-	}
-
-	function algo1(s){
-		var startPos = floor(bufferPos);
-		if (prevTime !== self.time){
-			calcCoeff();
-		}
-		bufferPos += speed;
-		if (bufferPos >= bufferSize){
-			bufferPos -= bufferSize;
-		}
-		fillBuffer(s + buffer[floor(bufferPos)] * self.feedback, startPos, floor(bufferPos));
-	}
-
-	this.time = !time ? 1000 : time; //ms
-	this.feedback = 0.0; // 0.0 - 1.0
-	this.algorithm = 0; // ['No resampling(faster)', 'Resampling (slower, beta)']
-	this.samplerate = samplerate;
-
-	this.pushSample = function(s){
-		if (this.algorithm === 0){
-			algo0(s);
-		} else {
-			algo1(s);
-		}
-		sample = buffer[floor(bufferPos)];
-		return sample;
-	};
-
-	this.getMix = function(){
-		return sample;
-	};
+function Delay(sampleRate, time, feedback){
+	var	self	= this;
+	self.time	= isNaN(time) ? self.time : time;
+	self.feedback	= isNaN(time) ? self.feedback : feedback;
+	self.reset(sampleRate);
 }
+
+Delay.prototype = {
+	/** Sample rate on which the Delay operatos on. */
+	sampleRate:	1,
+	/** Buffer position of the Delay. */
+	bufferPos:	0,
+	/** AudioBuffer in which the delay line is stored. */
+	buffer:		null,
+	/** Time between delays, in milliseconds. */
+	time:		1000,
+	/** Feedback of the Delay */
+	feedback:	0,
+	/** Current output of the Delay */
+	sample:		0,
+
+/**
+ * Reverse sample time factor
+ *
+ * @private
+*/
+	_rstf:		1,
+/**
+ * Adds a new sample to the delay line, moving the effect one sample forward in sample time.
+ *
+ * @param {Float32} sample The sample to be added to the delay line.
+ * @return {Float32} Current output of the Delay.
+*/
+	pushSample: function(s){
+		var	self	= this,
+			buffer	= self.buffer;
+		buffer[self.bufferPos++] += s;
+		if (self.bufferPos > self.time * self._rstf){
+			self.bufferPos = 0;
+		}
+		self.sample = buffer[self.bufferPos];
+		buffer[self.bufferPos] *= self.feedback;
+		return self.sample;
+	},
+/**
+ * Returns the current output of the Delay.
+ *
+ * @return {Float32} Current output of the Delay.
+*/
+	getMix: function(){
+		return this.sample;
+	},
+/**
+ * Changes the time value of the Delay and resamples the delay line accordingly.
+ *
+ * @param {Uint} time The new time value for the Delay.
+ * @return {AudioBuffer} The new delay line audio buffer.
+*/
+// Requires Sampler
+	resample: function(time){
+		var	self	= this,
+			ratio	= self.time / time;
+		self.buffer	= audioLib.Sampler.resample(self.buffer, time);
+		self.time	= time;
+		self.bufferPos	= Math.round(ratio * self.bufferPos);
+		return self.buffer;
+	},
+/**
+ * Resets the delay line, to recover from sample rate changes or such.
+ *
+ * @param {Number} sampleRate The new sample rate. (Optional)
+ * @param {Boolean} resample Determines whether to resample and apply the old buffer. (Requires Sampler)
+ * @return {AudioBuffer} The new delay line audio buffer.
+*/
+	reset: function(sampleRate, resample){
+		var	self	= this,
+			buf	= self.buffer,
+			i, ratio;
+		sampleRate	= isNaN(sampleRate) ? self.sampleRate : sampleRate;
+		ratio		= self.sampleRate / sampleRate;
+		self.buffer	= new Float32Array(sampleRate * Delay.MAX_DELAY);
+		self.bufferPos	= Math.round(ratio * self.bufferPos);
+		self._rstf	= 1 / 1000 * sampleRate;
+		if (resample){
+			buf = audioLib.Sampler.resample(buf, ratio);
+			for (i=0; i<buf.length && i<self.buffer.length; i++){
+				self.buffer[i] = buf[i];
+			}
+		}
+		return self.buffer;
+	}
+};
+
+/** The size that will be allocated for delay line buffers on initialization, in seconds */
+Delay.MAX_DELAY = 2;
