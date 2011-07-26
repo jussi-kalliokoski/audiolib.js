2ef87c0caa1d842afac62c3f94641de7216af00a
David Govea <govea.d@gmail.com>
2011-07-25 21:40:01 -0700

    Compressor effect

--- js/effects/extra.js
+++ js/effects/extra.js
@@ -28,4 +28,21 @@ audioLib.Expo = function(sampleRate, param){
 	};
 }
 
+audioLib.Compressor = function(sampleRate, scaleBy, gain){
+    var self    = this;
+        sample  = 0.0;
+    self.sampleRate = sampleRate;
+    self.scale = scaleBy || 1;
+    self.gain = gain || 0.5;
+    self.pushSample = function(s){
+        s /= self.scale;
+        sample = (1+self.gain)*s - self.gain*s*s*s;
+        return sample;
+    };
+    self.getMix = function(){
+        return sample;
+    };
+
+}
+
 }(this, audioLib));
