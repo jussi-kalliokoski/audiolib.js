a4f88a04a86f1fde167cf8bbe09af608397ac453
David Govea <govea.d@gmail.com>
2011-07-25 21:45:18 -0700

    Documentation

--- js/effects/extra.js
+++ js/effects/extra.js
@@ -28,6 +28,15 @@ audioLib.Expo = function(sampleRate, param){
 	};
 }
 
+/**
+ * Creates a Compressor Effect
+ * 
+ * @constructor
+ * @this {Compressor}
+ * @param {number} samplerate Sample Rate (hz).
+ * @param {number} scaleBy (Optional) Signal scaling factor. If mixing n unscaled waveforms, use scaleBy=n.
+ * @param {number} gain (Optional) Gain factor (1.0 - 2.0).
+*/
 audioLib.Compressor = function(sampleRate, scaleBy, gain){
     var self    = this;
         sample  = 0.0;
