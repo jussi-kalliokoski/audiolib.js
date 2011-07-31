e641e9b1c085a71d4c7925540ca59ab208a4ada3
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-07-31 13:41:08 +0300

    Rewrote Step Sequencer to work like a generator.

--- js/controls/stepsequencer.js
+++ js/controls/stepsequencer.js
@@ -6,42 +6,67 @@
  * @param {number} samplerate Sample Rate (hz).
  * @param {number} stepLength (Optional) Step Length (ms).
  * @param {Array} steps (Optional) Array of steps (unsigned double) for the sequencer to iterate.
- * @param {number} attack (Optional) Attack (ms).
+ * @param {number} attack (Optional) Attack.
 */
 function StepSequencer(sampleRate, stepLength, steps, attack){
 	var	self	= this,
 		phase	= 0;
 
-	self.sampleRate		= sampleRate;
-	self.stepLength		= stepLength || 200; // ms
-	self.steps		= steps || [1,0];
-	self.value		= 0;
-	self.attack		= attack || 0;
+	this.sampleRate		= isNaN(sampleRate) ? this.sampleRate : sampleRate;
+	this.stepLength		= isNaN(stepLength) ? this.stepLength : stepLength;
+	this.steps		= steps || [1, 0];
+	this.attack		= isNaN(attack) ? this.attack : attack;
+}
 
+StepSequencer.prototype = {
+	/** The sample rate of the step sequencer */
+	sampleRate:	44100,
+	/** The length a single step of the step sequencer, in ms */
+	stepLength:	200,
+	/** An array of the steps of the step sequencer */
+	steps:		null,
+	/** The current value of the step sequencer */
+	value:		0,
+	/** Transition time between steps, measured in steps */
+	attack:		0,
+	/** The current phase of the step sequencer */
+	phase:		0,
 
-	self.generate = function(){
-		var	stepLength	= self.sampleRate / 1000 * self.stepLength,
+/**
+ * Moves the step sequencer one sample further in sample time.
+ *
+ * @return {Number} The current value of the step sequencer.
+*/
+	generate: function(){
+		var	self		= this,
+			stepLength	= self.sampleRate / 1000 * self.stepLength,
 			steps		= self.steps,
 			sequenceLength	= stepLength * steps.length,
 			step, overStep, prevStep, stepDiff,
 			val;
-		if (++phase === sequenceLength){
-			phase = 0;
-		}
-		step		= phase / sequenceLength * steps.length;
+		self.phase	= (self.phase + 1) % sequenceLength;
+		step		= self.phase / sequenceLength * steps.length;
 		overStep	= step % 1;
 		step		= Math.floor(step);
 		prevStep	= (step || steps.length) - 1;
 		stepDiff	= steps[step] - steps[prevStep];
-		val = steps[step];
+		val		= steps[step];
 		if (overStep < self.attack){
 			val -= stepDiff - stepDiff / self.attack * overStep;
 		}
 		self.value = val;
 		return val;
+	},
+/**
+ * Returns the current value of the step sequencer.
+ *
+ * @return {Number} The current value of the step sequencer.
+*/
+	getMix: function(){
+		return this.value;
+	},
+/** Triggers the gate for the step sequencer, resetting its phase to zero. */
+	triggerGate: function(){
+		this.phase = 0;
 	}
-
-	self.triggerGate = function(){
-		phase = 0;
-	}
-}
+};
