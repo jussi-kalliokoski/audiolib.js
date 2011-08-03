65e612b356be878fda88e4c5ed8450ba0da5787d
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-08-03 16:52:32 +0300

    Added AudioProcessingUnit for buffer-based analyzer modules

---
+++ js/process/audioprocessingunit.js
@@ -0,0 +1,26 @@
+function AudioProcessingUnit(bufferSize){
+	var k;
+	for (k in AudioProcessingUnit.prototype){
+		if (AudioProcessingUnit.prototype.hasOwnProperty(k)){
+			this[k] = AudioProcessingUnit.prototype[k];
+		}
+	}
+	this.resetBuffer.apply(this, arguments);
+}
+
+AudioProcessingUnit.prototype = {
+	bufferPos:	-1,
+	pushSample: function(s){
+		this.bufferPos = (this.bufferPos + 1) % this.buffer.length;
+		this.bufferPos === 0 && this.process(this.buffer);
+		this.buffer[this.bufferPos] = s;
+	},
+	getMix: function(){
+		return this.buffer[this.bufferPos];
+	},
+	resetBuffer: function(bufferSize){
+		this.bufferSize	= isNaN(bufferSize) ? this.bufferSize : bufferSize;
+		this.buffer	= new Float32Array(this.bufferSize);
+		this.bufferPos	= -1;
+	}
+};
