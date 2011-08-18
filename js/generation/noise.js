e394c731122b1e2fa694bfb51f163272c949d743
root <root@euve1170.startvps.com>
2011-08-18 17:46:01 +0200

    Noise improvements

--- js/generation/noise.js
+++ js/generation/noise.js
@@ -7,37 +7,33 @@ Noise.prototype = {
 	sampleRate:	44100,
 	/* The color of the Noise. */
 	color:		'white',
-	q:		15,
+	b0:		0,
+	b1:		0,
+	b2:		0,
+	b3:		0,
+	b4:		0,
+	b5:		0,
 	c1:		null,
 	c2:		null,
 	c3:		null,
 	c4:		null,
+	q:		15,
+	q0:		null,
+	q1:		null,
 	/* Brown seed. */
 	brownQ:		0,
-	/* Pink seed. */
-	pinkQ:		0,
 	/* Current value of the Noise. */
 	value:		0,
-	/* The values of the octaves of the pink noise. */
-	pinkVals:	null,
-	/* The amount of octaves for the white noise. */
-	pinkPrecision:	16,
-	/* The amount of octaves for the white noise - 1. */
-	pinkPrecision1:	15,
-	/* The pink noise position counter. */
-	count:		1,
-	/* The length of the counter in bits. */
-	CTZBitCount:	64, // The maximum in JS, 2011
 	reset: function(sampleRate, color){
 		this.sampleRate		= isNaN(sampleRate) ? this.sampleRate : sampleRate;
 		this.color		= typeof color === 'string' ? color : this.color;
-		this.pinkVals		= new Float32Array(this.pinkPrecision);
-		this.pinkPrecision1	= this.pinkPrecision - 1;
 		this.c1			= (1 << this.q) - 1;
 		this.c2			= (~~(this.c1 /3)) + 1;
 		this.c3			= 1 / this.c1;
 		this.c1			= this.c2 * 6;
 		this.c4			= 3 * (this.c2 - 1);
+		this.q0			= Math.exp(-200 * Math.PI / this.sampleRate);
+		this.q1			= 1 - this.q0;
 	},
 	generate: function(){
 		this.value	= this[this.color]();
@@ -45,49 +41,24 @@ Noise.prototype = {
 	getMix: function(){
 		return this.value;
 	},
-	CTZ: function(n){
-		var i = 0;
-		while( (num >> i) & 1 === 0 && i < this.CTZBitCount ){
-			i++;
-		}
-		return i;
-	},
 	white: function(){
 		var r = Math.random();
 		return (r * this.c1 - this.c4) * this.c3;
 	},
 	pink: function(){
-		var	k	= this.CTZ(this.count) & this.pinkPrecision1,
-			p	= this.pinkVals[k],
-			r;
-
-		for(;;){
-			r			= this.white();
-			this.pinkVals[k]	= r;
-			r -= p;
-			this.pinkQ		+= r;
-			if (this.pinkQ < -4 || this.pinkQ > 4){
-				this.pinkQ	-= r;
-			} else {
-				break;
-			}
-		}
-
-		this.count++;
-
-		return (this.white() + this.pinkQ) * 0.125;
+		var	w	= this.white();
+		this.b0 = 0.997 * this.b0 + 0.029591 * w;
+		this.b1 = 0.985 * this.b1 + 0.032534 * w;
+		this.b2 = 0.950 * this.b2 + 0.048056 * w;
+		this.b3 = 0.850 * this.b3 + 0.090579 * w;
+		this.b4 = 0.620 * this.b4 + 0.108990 * w;
+		this.b5 = 0.250 * this.b5 + 0.255784 * w;
+		return 0.55 * (this.b0 + this.b1 + this.b2 + this.b3 + this.b4 + this.b5);
 	},
 	brown: function(){
-		var r;
-		for (;;){
-			r = this.white();
-			this.brownQ += r;
-			if (this.brownQ < -8 || this.brownQ > 8){
-				this.brownQ -= r;
-			} else {
-				break;
-			}
-		}
-		return this.brownQ * 0.0625;
+		var	w	= this.white(),
+			fc	= 100;
+		this.brownQ	= (this.q1 * w + this.q0 * this.brownQ);
+		return 6.2 * this.brownQ;
 	}
 };
