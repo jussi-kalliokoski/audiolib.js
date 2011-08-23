cc068f4e90db3f9a7e339b581d4e1ed5d69f57fb
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-08-23 18:09:58 +0200

    Fixed integration script errors.

--- integration/integrate.sh
+++ integration/integrate.sh
@@ -8,9 +8,9 @@ cat fft.js/fft.js >> ../js/process/fft.js
 echo "Integrating pcmdata.js..."
 cat prepend-pcmdata.js > ../js/io/pcmdata.js
 echo "\n" >> ../js/io/pcmdata.js
-cat pcmdata.js/lib/pcmdata.js >> ..js/io/pcmdata.js
+cat pcmdata.js/lib/pcmdata.js >> ../js/io/pcmdata.js
 
 echo "Integrating sink.js..."
-cat sink.js > ../js/io/sink.js
+cat sink.js/sink.js > ../js/io/sink.js
 
 echo "Integration script done."
