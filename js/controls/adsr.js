95796557d19f9a3a9d8f2cb3f9fee14aaadf9987
root <root@euve1170.startvps.com>
2011-08-23 15:49:11 +0200

    Added static ADSR discussed in issue #24

--- js/controls/adsr.js
+++ js/controls/adsr.js
@@ -8,13 +8,17 @@
  * @param {number} decay (Optional) Decay (ms).
  * @param {number} sustain (Optional) Sustain (unsigned double).
  * @param {number} release (Optional) Release (ms).
+ * @param {number} sustainTime (Optional) The time the sustain mode lasts (ms).
+ * @param {number} releaseTime (Optional) The time the release mode lasts (ms).
 */
-function ADSREnvelope(sampleRate, attack, decay, sustain, release){
-	this.sampleRate	= isNaN(sampleRate) ? this.sampleRate : sampleRate;
-	this.attack	= isNaN(attack) ? this.attack : attack;
-	this.decay	= isNaN(decay) ? this.decay : decay;
-	this.sustain	= isNaN(sustain) ? this.sustain : sustain;
-	this.release	= isNaN(release) ? this.release : release;
+function ADSREnvelope(sampleRate, attack, decay, sustain, release, sustainTime, releaseTime){
+	this.sampleRate		= isNaN(sampleRate) ? this.sampleRate : sampleRate;
+	this.attack		= isNaN(attack) ? this.attack : attack;
+	this.decay		= isNaN(decay) ? this.decay : decay;
+	this.sustain		= isNaN(sustain) ? this.sustain : sustain;
+	this.release		= isNaN(release) ? this.release : release;
+	this.sustainTime	= isNaN(sustainTime) ? null : sustainTime;
+	this.releaseTime	= isNaN(releaseTime) ? null : releaseTime;
 }
 
 ADSREnvelope.prototype = {
@@ -34,6 +38,16 @@ ADSREnvelope.prototype = {
 	state:		3,
 	/** The state of the gate of the envelope, open being true */
 	gate:		false,
+	/** The time the sustain phase should be sustained before launching release. If null, will wait for triggerGate. */
+	sustainTime:	null,
+	/** The time the release phase should be sustained before relaunching attack. If null, will wait for triggerGate. */
+	releaseTime:	null,
+/**
+ * Private variable for timing the timed sustain and release.
+ *
+ * @private
+*/
+	_st: 0,
 /**
  * Moves the envelope status one sample further in sample-time.
  *
@@ -57,8 +71,10 @@ ADSREnvelope.prototype = {
  * @param {Boolean} isOpen The new state of the gate.
 */
 	triggerGate: function(isOpen){
+		isOpen		= typeof isOpen === 'undefined' ? !this.gate : isOpen;
 		this.gate	= isOpen;
-		this.state	= isOpen ? 0 : 3;
+		this.state	= isOpen ? 0 : this.releaseTime === null ? 3 : 5;
+		this._st	= 0;
 	},
 /**
  * Array of functions for handling the different states of the envelope.
@@ -73,7 +89,12 @@ ADSREnvelope.prototype = {
 		function(){ // Decay
 			this.value -= 1000 / this.sampleRate / this.decay * this.sustain;
 			if (this.value <= this.sustain){
-				this.state = 2;
+				if (this.sustainTime === null){
+					this.state	= 2;
+				} else {
+					this._st	= 0;
+					this.state	= 4;
+				}
 			}
 		},
 		function(){ // Sustain
@@ -81,6 +102,20 @@ ADSREnvelope.prototype = {
 		},
 		function(){ // Release
 			this.value = Math.max(0, this.value - 1000 / this.sampleRate / this.release);
+		},
+		function(){ // Timed sustain
+			this.value = this.sustain;
+			if (this._st++ >= this.sampleRate * 0.001 * this.sustainTime){
+				this._st	= 0;
+				this.state	= this.releaseTime === null ? 3 : 5;
+			}
+		},
+		function(){ // Timed release
+			this.value = Math.max(0, this.value - 1000 / this.sampleRate / this.release);
+			if (this._st++ >= this.sampleRate * 0.001 * this.releaseTime){
+				this._st	= 0;
+				this.state	= 0;
+			}
 		}
 	]
 };
