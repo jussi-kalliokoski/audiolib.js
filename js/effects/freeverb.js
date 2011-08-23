8a47e36fe42a39473cdee8cfe87efa1c90ec424e
David Govea <govea.d@gmail.com>
2011-08-23 14:05:27 -0700

    Made Freeverb a multi-channel effect

--- js/effects/freeverb.js
+++ js/effects/freeverb.js
@@ -7,92 +7,117 @@
  * @param {boolean} isRightChannel Controls the addition of stereo spread. Defaults to false.
  * @param {Object} tuning (Optional) Freeverb tuning overwrite object
 */
-function Freeverb(sampleRate, isRightChannel, tuning){
-	var	self	= this,
-		sample  = 0.0;
-	tuning		= tuning || Freeverb.tuning;
-	self.sampleRate	= sampleRate;
-	self.spread	= isRightChannel ? tuning.stereospread : 0;
-	self.wet	= 0.5;
-	self.dry	= 0.55;
-	self.inScale	= tuning.fixedgain;
+function Freeverb(sampleRate, channelCount, tuning){
+	var	self		= this;
+	self.sampleRate		= sampleRate;
+	self.channelCount	= channelCount || self.channelCount;
+	self.tuning		= tuning || this.tuning;
+	
+	self.sample	= (function(){
+		var	sample	= [],
+			c;
+		for(c=0; c<self.channelCount; c++){
+			sample[c] = 0.0;
+		}
+		return sample;
+	}());
+
 	self.CFs	= (function(){
 		var 	combs	= [],
-			num	= tuning.numcombs,
-			damp	= tuning.initialdamp * tuning.scaledamp,
-			feed	= tuning.initialroom * tuning.scaleroom + tuning.offsetroom,
-			sizes	= tuning.combs,
-			i;
-		for(i=0; i<num; i++){
-			combs.push(new audioLib.CombFilter(self.sampleRate, sizes[i] + self.spread, feed, damp));
+			channel	= [],
+			num	= self.tuning.numcombs,
+			damp	= self.tuning.initialdamp * self.tuning.scaledamp,
+			feed	= self.tuning.initialroom * self.tuning.scaleroom + self.tuning.offsetroom,
+			sizes	= self.tuning.combs,
+			i, c;
+		for(c=0; c<self.channelCount; c++){
+			for(i=0; i<num; i++){
+				channel.push(new audioLib.CombFilter(self.sampleRate, sizes[i] + c * self.tuning.stereospread, feed, damp));
+			}
+			combs.push(channel);
+			channel = [];
+			console.log(c * self.tuning.stereospread);
 		}
+		console.log(self.tuning);
 		return combs;
 	}());
 	self.numCFs	= self.CFs.length;
 	
 	self.APFs	= (function(){
 		var 	apfs	= [],
-			num	= tuning.numallpasses,
-			feed	= 0.5,
-			sizes	= tuning.allpasses,
-			i;
-		for(i=0; i<num; i++){
-			apfs.push(new Freeverb.AllPassFilter(self.sampleRate, sizes[i] + self.spread, feed));
+			channel	= [],
+			num	= self.tuning.numallpasses,
+			feed	= self.tuning.allpassfb,
+			sizes	= self.tuning.allpasses,
+			i, c;
+		for(c=0; c<self.channelCount; c++){
+			for(i=0; i<num; i++){
+				channel.push(new Freeverb.AllPassFilter(self.sampleRate, sizes[i] + c * self.tuning.stereospread, feed));
+			}
+			apfs.push(channel);
+			channel = [];
 		}
 		return apfs;
 	}());
 	self.numAPFs	= self.APFs.length;
-
-	self.pushSample	= function(s){
-		var	input	= s * self.inScale,
-			output	= 0,
-			i;
-		for(i=0; i < self.numCFs; i++){
-			output += self.CFs[i].pushSample(input);
-		}
-		for(i=0; i < self.numAPFs; i++){
-			output = self.APFs[i].pushSample(output);
-		}
-		sample = output * self.wet + s * self.dry;
-		return sample;
-	};
-	
-	self.getMix = function(){
-		return sample;
-	};
-	
-	self.reset = function(){
-		var	i;		
-		for(i=0; i < self.numCFs; i++){
-			self.CFs[i].reset();
-		}
-		for(i=0; i < self.numAPFs; i++){
-			self.APFs[i].reset();
-		}
-		sample	= 0.0;
-	};
+	console.log(self);
 }
 
-// Tuning from FreeVerb source. Much of this is unused. Will optimize further once stereo effect spec makes progress
-Freeverb.tuning = {
-	numcombs:	8,
-	combs:		[1116, 1188, 1277, 1356, 1422, 1491, 1557, 1617],
-	numallpasses:	4,
-	allpasses:	[556, 441, 341, 225],
-	fixedgain:	0.015,
-	scalewet:	3,
-	scaledry:	2,
-	scaledamp:	0.4,
-	scaleroom:	0.28,
-	offsetroom:	0.7,
-	initialroom:	0.5,
-	initialdamp:	0.5,
-	initialwet:	1/3,
-	initialdry:	0,
-	initialwidth:	1,
-	initialmode:	0,
-	freezemode:	0.5,
-	stereospread:	23
+Freeverb.prototype = {
+	channelCount: 	2,
+	sample:		[0.0, 0.0],
+
+	wet:		0.5,
+	dry:		0.55,
+
+	tuning: {
+		numcombs:	8,
+		combs:		[1116, 1188, 1277, 1356, 1422, 1491, 1557, 1617],
+
+		numallpasses:	4,
+		allpasses:	[556, 441, 341, 225],
+		allpassfb:	0.5,
+
+		fixedgain:	0.015,
+		scaledamp:	0.4,
+		scaleroom:	0.28,
+		offsetroom:	0.7,
+		initialroom:	0.5,
+		initialdamp:	0.5,
+		stereospread:	23
+	},
+
+	pushSample: function(s, channel){
+		var	input	= s * this.tuning.fixedgain,
+			output	= 0,
+			i;
+		for(i=0; i < this.numCFs; i++){
+			output += this.CFs[channel][i].pushSample(input);
+		}
+		for(i=0; i < this.numAPFs; i++){
+			output = this.APFs[channel][i].pushSample(output);
+		}
+		this.sample[channel] = output * this.wet + s * this.dry;
+	},
+
+	getMix: function(channel){
+		return this.sample[channel];
+	},
+
+	reset: function(){
+		var	i;		
+		for(i=0; i < this.numCFs; i++){
+			this.CFs[i].reset();
+		}
+		for(i=0; i < this.numAPFs; i++){
+			this.APFs[i].reset();
+		}
+		for(i=0; i < this.channelCount; i++){
+			this.sample[i] = 0.0;
+		}
+	}
+
+	
 };
 
 /**
