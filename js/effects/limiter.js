0f8e5c6b2ef639c74e039a1ee1346b18ff523279
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-09-13 14:00:17 +0200

    Added documentation for Limiter and Amplitude.

--- js/effects/limiter.js
+++ js/effects/limiter.js
@@ -1,3 +1,15 @@
+/**
+ * Creates a dynamic amplitude limiter.
+ *
+ * Requires Amplitude.
+ *
+ * @constructor
+ * @this Limiter
+ * @param {Number} sampleRate The sample rate of the limiter.
+ * @param {Number} threshold The amplitude threshold after which to start limiting.
+ * @param {Number} attack The speed on which the amplitude metering reacts.
+ * @param {Number} decay The speed on which the amplitude metering cools down.
+*/
 function Limiter(sampleRate, threshold, attack, decay){
 	this.sampleRate		= isNaN(sampleRate) ? this.sampleRate : sampleRate;
 	this.threshold		= isNaN(threshold) ? this.threshold : threshold;
@@ -7,19 +19,45 @@ function Limiter(sampleRate, threshold, attack, decay){
 }
 
 Limiter.prototype = {
+	/** The sample rate of the effect. */
 	sampleRate:	44100,
+	/** The Amplitude meter on which the limiting is based. */
+	__amplitude:	null,
+	/** The amplitude threshold after which to start limiting. */
 	threshold:	0.95,
+	/** The speed on which the amplitude metering reacts. */
 	attack:		0.01,
+	/** The speed on which the amplitude metering cools down. */
 	release:	0.01,
+	/** The current output of the effect. */
 	sample:		0,
+/**
+ * Processes a sample, moving the effect one sample further in sample-time.
+ *
+ * @param {Float32} sample The sample to process.
+ * @param {Uint} channel The channel on which the sample is. (Only if multi-channel)
+ * @return {Float32} The current output of the effect. (Only if single-channel)
+*/
 	pushSample: function(s){
 		var	d	= this._amplitude.pushSample(s) - this.threshold;
 		this.sample	= d > 0 ? s / (1 + d) : s;
 		return this.sample;
 	},
+/**
+ * Returns the current output of the effect.
+ *
+ * @param {Uint} channel The channel for which to get the sample.
+ * @return {Float32} The current output of the effect.
+*/
 	getMix: function(){
 		return this.sample;
 	},
+/**
+ * Sets a parameter of the effect, making necessary relative calculations.
+ *
+ * @param {String} param The parameter name.
+ * @param {Object} value The new value of the parameter.
+*/
 	setParam: function(param, value){
 		switch(param){
 		case 'attack':
--- js/process/amplitude.js
+++ js/process/amplitude.js
@@ -1,3 +1,12 @@
+/**
+ * Creates an amplitude meter, outputting the amplitude value of the input.
+ *
+ * @constructor
+ * @this Limiter
+ * @param {Number} sampleRate The sample rate of the amplitude meter.
+ * @param {Number} attack The speed on which the amplitude metering reacts.
+ * @param {Number} decay The speed on which the amplitude metering cools down.
+*/
 function Amplitude(sampleRate, attack, decay){
 	this.sampleRate		= isNaN(sampleRate) ? this.sampleRate : sampleRate;
 	this.attack		= isNaN(attack) ? this.attack : attack;
@@ -5,14 +14,31 @@ function Amplitude(sampleRate, attack, decay){
 }
 
 Amplitude.prototype = {
+	/** The sample rate of the effect. */
 	sampleRate:	44100,
+	/** The speed on which the amplitude metering reacts. */
 	attack:		0.01,
+	/** The speed on which the amplitude metering cools down. */
 	release:	0.01,
+	/** The current output of the effect. */
 	sample:		0,
+/**
+ * Processes a sample, moving the effect one sample further in sample-time.
+ *
+ * @param {Float32} sample The sample to process.
+ * @param {Uint} channel The channel on which the sample is. (Only if multi-channel)
+ * @return {Float32} The current output of the effect. (Only if single-channel)
+*/
 	pushSample: function(s){
 		this.sample = Math.abs((s > this.sample ? this.attack : this.release) * (this.sample - s) + s);
 		return this.sample;
 	},
+/**
+ * Returns the current output of the effect.
+ *
+ * @param {Uint} channel The channel for which to get the sample.
+ * @return {Float32} The current output of the effect.
+*/
 	getMix: function(){
 		return this.sample;
 	},
