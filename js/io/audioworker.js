b0df9dbbdc8cf045e523878e4e9f5c480af883d3
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-12-05 00:23:01 +0200

    Updated AudioWorker to use Sink.inlineWorker

--- js/io/audioworker.js
+++ js/io/audioworker.js
@@ -1,4 +1,7 @@
 (function(){
+
+/* Depends on Sink.inlineWorker */
+
 function inject(){
 	var	args	= arguments,
 		l	= args.length,
@@ -10,21 +13,14 @@ function inject(){
 }
 
 audioLib.AudioWorker = function(code, injectable){
-	var	blob	= new (window.MozBlobBuilder || window.BlobBuilder)(),
-		url, worker;
-	blob.append('var audioLib = (' + String(AUDIOLIB) + '(this, Math, Object, Array));\n');
-	for (url = 0; url < audioLib.plugins._pluginList.length; url++){
-		blob.append('(' + String(audioLib.plugins._pluginList[url]) + '());\n');
+	var	worker	= 'var audioLib=(' + String(AUDIOLIB) + '(this, Math, Object, Array));\n'),
+		i;
+	for (i=0; i < audioLib.plugins._pluginList.length; i++) {
+		worker += '(' + String(audioLib.plugins._pluginList[url]) + '());\n';
 	}
-	injectable && blob.append('this.addEventListener("message",function(e){e.data&&e.data.type==="injection"&&Function(e.data.code).call(this)}, true);\n');
-	blob.append(code instanceof Function ? '(' + String(code) + ').call(this);' : code);
-	url	= window.URL.createObjectURL(blob.getBlob());
-	worker	= new Worker(url);
-	worker._terminate	= worker.terminate;
-	worker.terminate	= function(){
-		window.URL.revokeObjectURL(id);
-		return worker._terminate.call(worker, arguments);
-	};
+	injectable && (worker += 'this.addEventListener("message",function(e){e.data&&e.data.type==="injection"&&Function(e.data.code).call(this)},true);\n');
+	worker += (code instanceof Function ? '(' + String(code) + ').call(this);' : code);
+	worker = Sink.inlineWorker(worker);
 	if (injectable){
 		worker.inject = inject;
 	}
