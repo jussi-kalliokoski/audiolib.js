818f1f2333ea43a34ee494d00f941e5127f40a6a
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-01-12 21:56:36 +0200

    Updated sink.js

--- src/io/sink.js
+++ src/io/sink.js
@@ -505,6 +505,103 @@ Sink.sinks('moz', function () {
 });
 
 }(this.Sink));
+(function (Sink, sinks) {
+
+var sinks = Sink.sinks;
+
+function newAudio (src) {
+	var audio = document.createElement('audio');
+	if (src) {
+		audio.src = src;
+	}
+	return audio;
+}
+
+/* TODO: Implement a <BGSOUND> hack for IE8.
+
+/**
+ * A sink class for WAV data URLs
+ * Relies on pcmdata.js and utils to be present.
+ * Thanks to grantgalitz and others for the idea.
+*/
+sinks('wav', function () {
+	var	self			= this,
+		audio			= new sinks.wav.wavAudio,
+		PCMData			= typeof PCMData === 'undefined' ? audioLib.PCMData : PCMData;
+	self.start.apply(self, arguments);
+	var	soundData		= new Float32Array(self.bufferSize * self.channelCount),
+		zeroData		= new Float32Array(self.bufferSize * self.channelCount);
+
+	if (!newAudio().canPlayType('audio/wav; codecs=1') || !btoa) throw 0;
+	
+	function bufferFill () {
+		if (self._audio.hasNextFrame) return;
+		Sink.memcpy(zeroData, 0, soundData, 0);
+		self.process(soundData, self.channelCount);
+
+		self._audio.setSource('data:audio/wav;base64,' + btoa(
+			audioLib.PCMData.encode({
+				data:		soundData,
+				sampleRate:	self.sampleRate,
+				channelCount:	self.channelCount,
+				bytesPerSample:	self.quality,
+			})
+		));
+
+		if (!self._audio.currentFrame.src) self._audio.nextClip();
+	}
+	
+	self.kill		= Sink.doInterval(bufferFill, 40);
+	self._bufferFill	= bufferFill;
+	self._audio		= audio;
+}, {
+	quality: 1,
+	bufferSize: 22050,
+
+	getPlaybackTime: function () {
+		var audio = this._audio;
+		return (audio.currentFrame ? audio.currentFrame.currentTime * this.sampleRate : 0) + audio.samples;
+	},
+});
+
+function wavAudio () {
+	var self = this;
+
+	self.currentFrame	= newAudio();
+	self.nextFrame		= newAudio();
+
+	self._onended		= function () {
+		self.samples += self.bufferSize;
+		self.nextClip();
+	};
+}
+
+wavAudio.prototype = {
+	samples:	0,
+	nextFrame:	null,
+	currentFrame:	null,
+	_onended:	null,
+	hasNextFrame:	false,
+
+	nextClip: function () {
+		var	curFrame	= this.currentFrame;
+		this.currentFrame	= this.nextFrame;
+		this.nextFrame		= curFrame;
+		this.hasNextFrame	= false;
+		this.currentFrame.play();
+	},
+
+	setSource: function (src) {
+		this.nextFrame.src = src;
+		this.nextFrame.addEventListener('ended', this._onended, true);
+
+		this.hasNextFrame = true;
+	},
+};
+
+sinks.wav.wavAudio = wavAudio;
+
+}(this.Sink));
 (function (Sink) {
 
 /**
