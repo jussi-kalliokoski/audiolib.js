c57e8a3bb5d65ad3c768a8d745eea429f56a7fe1
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-04-24 23:06:39 +0300

    Set up grunt, see #64

---
+++ grunt.js
@@ -0,0 +1,36 @@
+/*jshint asi:true */
+module.exports = function (grunt) {
+
+function insert (arr, pos, arr2) {
+	return arr.splice.apply(arr, [pos, 0].concat(arr2))
+}
+
+var config = {
+	SOURCE : ['src/api-*.js', 'src/**/**/*.js'],
+	TEMPLATES : [/* SOURCE, */ 'templates/', 'build'],
+	WRAPPERS : ['src/wrapper-start.js', 'src/wrapper-end.js'],
+
+	IN: [/* WRAPPERS <- SOURCE*/],
+	OUT : 'lib/audiolib.js',
+	DOCS : 'lib/docs.html',
+	OUT_MIN : 'lib/audiolib.min.js',
+	RELEASE_TAR_GZ : 'audiolib.js.tar.gz',
+	RELEASE_ZIP : 'audiolib.js.zip',
+	PACKAGE : 'package.json',
+
+	concat: {
+		dist: {
+			src: '<config:IN>',
+			dest: '<config:OUT>'
+		}
+	}
+}
+
+insert(config.TEMPLATES, 0, config.SOURCE)
+insert(config.IN, 0, config.WRAPPERS)
+insert(config.IN, 1, config.SOURCE)
+
+grunt.initConfig(config)
+grunt.registerTask('default', 'concat')
+
+}
